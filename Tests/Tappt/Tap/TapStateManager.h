0ad0048ac751b106c75e5c42080e5257cc06c06a
John Kalberer <jlkalberer@gmail.com>
2018-01-19 19:50:14 -0800

    Removing test code -- leave it in a separate branch for now.

--- Tests/Tappt/Tap/TapStateManager.h
+++
@@ -1,6 +0,0 @@
-#ifndef TapStateManager_h
-#define TapStateManager_h
-
-#include "Tappt/KegeratorState/IStateManager.h"
-
-#endif
--- Tests/system/Servo.cpp
+++
@@ -1,7 +0,0 @@
-
-
-#include "Servo.h"
-
-DEFINE_FAKE_VOID_FUNC( Servo__write, Servo*, int );
-DEFINE_FAKE_VOID_FUNC( Servo__attach, Servo*, int );
-
